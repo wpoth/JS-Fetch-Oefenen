059a25b2faf05f35567ff39b4289e7f27922d7d8
wpoth <302758116@student.rocmondriaan.nl>
2023-12-08 17:55:06 +0100

    BOOOOOM

--- js/main.js
+++ js/main.js
@@ -1,21 +1,30 @@
-const ulList = document.querySelector("ul");
+// const ulList = document.querySelector("ul");
 
-fetch("https://pokeapi.co/api/v2/pokemon/?limit=10")
+// fetch("https://pokeapi.co/api/v2/pokemon/?limit=10")
+//   .then((Response) => Response.json())
+//   .then((data) => {
+//     console.log("Data:", data);
+//     const pokemons = data.results;
+
+//     for (let i = 0; i < pokemons.length; i++) {
+//       const pokemon = pokemons[i];
+//       ulList.innerHTML += `<li>${pokemon.name}</li>`;
+//     }
+//   });
+
+const cards = document.querySelector(".ulList1");
+
+fetch("https://dummyjson.com/products")
   .then((Response) => Response.json())
   .then((data) => {
     console.log("Data:", data);
-    const pokemons = data.results;
+    const products = data.products;
 
-    for (let i = 0; i < pokemons.length; i++) {
-      const pokemon = pokemons[i];
-      ulList.innerHTML += `<li>${pokemon.name}</li>`;
+    for (let i = 0; i < products.length; i++) {
+      const product = products[i];
+      cards.innerHTML +=`<div class="card">${product.title}
+      <img src="${product.thumbnail}"
+      <h3>&euro;${product.price}</h3> 
+      </div> `
     }
-  });
-
-
-  fetch("https://dummyjson.com/products")
-  .then((Response) => Response.json())
-  .then((data) => {
-    console.log("Data:", data);
-    const dummy = dummy.products;
   });
